faddf0362d98add5c14df2ba5a1181da18cf1a5e
Diogo Martins <diogo.felix.martins@gmail.com>
2025-06-11 12:25:09 +0100

    wip

--- lib/helpdesk/support/user.ex
+++ lib/helpdesk/support/user.ex
@@ -5,28 +5,41 @@ defmodule Helpdesk.Support.User do
     type "user"
 
     routes do
-      index :read, route: "/generic"
-      route(:post, "/generic", :submit)
+      index :read, route: "/read"
+      route(:get, "/generic", :submit)
+      # route(:post, "/generic", :submit)
     end
   end
 
   attributes do
     integer_primary_key :id, writable?: true, public?: true
     attribute :name, :string, public?: true
+    attribute :date, :naive_datetime, public?: true
+
+    create_timestamp :created_at, type: :naive_datetime, public?: true
   end
 
   actions do
+    defaults create: :*
+
     read :read do
       primary? true
     end
 
-    action :submit do
-      argument :orders, {:array, Helpdesk.Type.Order}, allow_nil?: true
+    action :submit, :struct do
+      constraints instance_of: __MODULE__
 
       run fn input, context ->
-        dbg(input.arguments.orders)
-        :ok
+        Action.run()
       end
     end
+  end
+end
+
+defmodule Action do
+  def run() do
+    {:ok, %Helpdesk.Support.User{
+      date: NaiveDateTime.utc_now()
+    }}
   end
 end
